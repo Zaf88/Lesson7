5c3bf797aaea303272a06218b138e52dd51ff674
Your Name <applezafar@gmail.com>
2022-06-08 19:19:12 +1000

    HOMEWORK7

--- src/Main.java
+++ src/Main.java
@@ -11,9 +11,10 @@ public class Main {
     public static void main(String[] args) {
         defineLeapYear(2000);
         defineOs(1);
-        defineVersion(2017, 1);}
-
-
+        defineVersion(2017, 1);
+        defineDeliveryTime(60);
+        defineDouble("abcdefghijk");
+    }
 
     public static void defineOs(int clientOs) {
         if (clientOs == 0) {
@@ -21,22 +22,40 @@ public class Main {
         }
         if (clientOs == 1) {
             System.out.println("Follow the link to install Android app");
-        }}
-    public static void defineVersion (int clientDeviceYear, int clientOs) {
+        }
+    }
+
+    public static void defineVersion(int clientDeviceYear, int clientOs) {
         if (clientDeviceYear < 2015 && clientOs == 0) {
             System.out.println("Install the light version of the IOS app from the link");
-        }
-        else if (clientDeviceYear >= 2015 && clientOs == 0) {
+        } else if (clientDeviceYear >= 2015 && clientOs == 0) {
             System.out.println("Install the Standard version of the IOS app from the link");
         }
         if (clientDeviceYear < 2015 && clientOs == 1) {
             System.out.println("Install the light version of the Android app from the link");
-        }
-        else if (clientDeviceYear >= 2015 && clientOs == 1){
+        } else if (clientDeviceYear >= 2015 && clientOs == 1) {
             System.out.println("Install the Standard version of the Android app from the link");
-    }}}
+        }
+    }
 
+    public static void defineDeliveryTime(int deliveryDistance) {
 
+        if (deliveryDistance < 20) {
+            System.out.println("DeliveryTime  1 day");
+        }
+        if (deliveryDistance >= 20) {
+            System.out.println("DeliveryTime  1 day +24 hours");
+        } else if (deliveryDistance >= 60) {
+            System.out.println("DeliveryTime  1 day +  48 hours");
+        }
+    }
 
-
-
+    public static void defineDouble (String str) {
+            for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                System.out.println("There is a double symbol" + str.charAt(i));
+                break;
+            }
+            if (i == str.length()-2) {
+                System.out.println("There are no doubles");
+            }}}}
